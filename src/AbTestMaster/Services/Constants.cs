c4fb4b6616e3efd6f7ee8e06cfc45dcb81322c4e
Jim Macaraeg <jim.macaraeg@lbox.com>
2014-02-14 09:58:32 -0800

    lighter weight cookies

--- src/AbTestMaster/Services/Constants.cs
+++ src/AbTestMaster/Services/Constants.cs
@@ -2,18 +2,18 @@
 {
     internal class Constants
     {
-        internal const string AB_TEST_MASTER_SESSION = "AB_TEST_MASTER_SESSION";
-        internal const string AB_TEST_MASTER_COOKIE = "AB_TEST_MASTER_COOKIE";
+        internal const string AB_TEST_MASTER_SESSION = "AB_SESSION";
+        internal const string AB_TEST_MASTER_COOKIE = "AB_COOKIE";
 
         internal const string ACTION = "action";
         internal const string CONTROLLER = "controller";
         internal const string AREA = "area";
 
-        internal const string SPLIT_GROUP = "SPLIT_GROUP";
-        internal const string SPLIT_NAME = "SPLIT_NAME";
-        internal const string SPLIT_SEQUENCE = "SPLIT_SEQUENCE";
-        internal const string SPLIT_VIEWS_SEQUENCE_TRAIL = "SPLIT_VIEWS_SEQUENCE_TRAIL";
-        internal const string SPLIT_GOAL = "SPLIT_GOAL";
+        internal const string SPLIT_GROUP = "GROUP";
+        internal const string SPLIT_NAME = "NAME";
+        internal const string SPLIT_SEQUENCE = "SEQUENCE";
+        internal const string SPLIT_VIEWS_SEQUENCE_TRAIL = "SEQUENCE_TRAIL";
+        internal const string SPLIT_GOAL = "GOAL";
 
         internal const string DATE_TIME = "DATE_TIME";
         internal const string SPLIT_GOALS_FILE_PATH = @"\App_Data\AB_TEST_MASTER_SplitGoals.csv";
